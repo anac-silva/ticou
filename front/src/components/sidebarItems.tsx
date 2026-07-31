2cdef73fdbba98e7081ce5e5e32ee205dab921b9
Ana Carolina Nunes Merloti <ana.carolina3551@gmail.com>
2026-07-30 22:48:37 -0300

    style: estilizando a página principal

--- front/src/components/sidebarItems.tsx
+++ front/src/components/sidebarItems.tsx
@@ -1,15 +1,15 @@
-
+import type { LucideIcon } from "lucide-react";
 
 type SidebarItemProps = {
     icon: LucideIcon;
     label: string;
 }
 
-export function SidebarItem({ icon, label }: SidebarItemProps){
+export function SidebarItem({ icon: Icon, label }: SidebarItemProps){
     return(
-        <div>
-            {icon}
-            <span>{label}</span>
+        <div className="flex justify-center">
+            <Icon className="my-3"/>
+            <span className="my-3 mx-2">{label}</span>
         </div>
     )
 }
