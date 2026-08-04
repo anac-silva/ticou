6f06ba13493ba088201b50ca659888d6ac84f6f0
Ana Carolina Nunes Merloti <ana.carolina3551@gmail.com>
2026-08-03 22:12:37 -0300

    feat: adiciona useState no front

--- front/src/components/taskform.tsx
+++ front/src/components/taskform.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
 
 export function Formulario() {
+    const [atividade, setAtividade] = useState("");
     return(
         <>
             <form action="#">
-                <input type="text" placeholder="Digite a atividade..." className="w-md py-2 px-2 rounded-lg border-2 border-gray-300 focus:border-[#ADB3FF] focus:outline-none"/>
+                <input 
+                type="text" 
+                placeholder="Digite a atividade..."
+                value={atividade}
+                onChange={(event) => setAtividade(event.target.value)} 
+                className="w-md py-2 px-2 rounded-lg border-2 border-gray-300 focus:border-[#ADB3FF] focus:outline-none"/>
                 <button type="submit" className="py-2 px-2 rounded-lg border-2 bg-[#ADB3FF] border-transparent mx-2">Salvar</button>
             </form>
         </>
